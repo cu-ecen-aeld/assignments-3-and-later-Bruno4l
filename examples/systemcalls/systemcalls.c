8a91639c95ff43338a9c3e0119899d2e0ff2aaa3
Bruno4l <brunocastellani@live.com>
2024-12-25 21:47:10 -0300

    assignment-3-part-1

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -16,8 +16,13 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
+    if (!cmd) {
+        return false;
+    }
 
-    return true;
+    int ret = system(cmd);
+    return (ret != -1 && WIFEXITED(ret) && WEXITSTATUS(ret) == 0);
+
 }
 
 /**
@@ -47,7 +52,7 @@ bool do_exec(int count, ...)
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
-    command[count] = command[count];
+    //command[count] = command[count];
 
 /*
  * TODO:
@@ -59,9 +64,24 @@ bool do_exec(int count, ...)
  *
 */
 
-    va_end(args);
+    pid_t pid = fork();
+    if (pid == -1) {
+        va_end(args);
+        return false; // Fork failed
+    }
+    else if (pid == 0) {
+        execv(command[0], command);
+        _exit(EXIT_FAILURE); // If execv fails
+    }
 
-    return true;
+    int status;
+    if (waitpid(pid, &status, 0) == -1) {
+        va_end(args);
+        return false;
+    }
+
+    va_end(args);
+    return WIFEXITED(status) && WEXITSTATUS(status) == 0;
 }
 
 /**
@@ -82,7 +102,7 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
-    command[count] = command[count];
+    //command[count] = command[count];
 
 
 /*
@@ -93,7 +113,38 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *
 */
 
-    va_end(args);
+    if (!outputfile) {
+        va_end(args);
+        return false;
+    }
 
-    return true;
+    pid_t pid = fork();
+    if (pid == -1) {
+        va_end(args);
+        return false; // Fork failed
+    }
+    else if (pid == 0) {
+        int fd = open(outputfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        if (fd == -1) {
+            _exit(EXIT_FAILURE);
+        }
+
+        if (dup2(fd, STDOUT_FILENO) == -1 || dup2(fd, STDERR_FILENO) == -1) {
+            close(fd);
+            _exit(EXIT_FAILURE);
+        }
+        close(fd);
+
+        execv(command[0], command);
+        _exit(EXIT_FAILURE); // If execv fails
+    }
+
+    int status;
+    if (waitpid(pid, &status, 0) == -1) {
+        va_end(args);
+        return false;
+    }
+
+    va_end(args);
+    return WIFEXITED(status) && WEXITSTATUS(status) == 0;
 }
