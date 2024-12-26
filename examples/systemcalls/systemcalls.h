0ab38d5bb729fd9b01d549f70c9c1b125e6dc5f5
Bruno4l <brunocastellani@live.com>
2024-12-25 21:49:32 -0300

    Add includes

--- examples/systemcalls/systemcalls.h
+++ examples/systemcalls/systemcalls.h
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdarg.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <cerrno>
+#include <cstring>
+#include <iostream>
 
 bool do_system(const char *command);
 
